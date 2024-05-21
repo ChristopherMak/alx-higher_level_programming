7cf360c7c5053a11470441b7b023fdb8f7a75594
ChristopherMak <makeketachristopher@gmail.com>
2024-05-21 20:06:50 +0200

    Updated 0-readme.js file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-// Read from file
+// Reads and prints the content of a file
 
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8',
-  function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
+const argv = process.argv;
+let fs = require('fs');
+fs.readFile(argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
     console.log(data);
-  });
+  }
+});
